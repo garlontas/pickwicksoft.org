6c3a17142bac49f1219813a6837dd0c9ab93fb51
Stefan Garlonta <stefan@pickwicksoft.org>
2024-04-24 15:17:54 +0200

    Many improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './remark-reading-time.mjs'
 
-import icon from "astro-icon";
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), icon()]
-});
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap(), tailwind(), icon()],
+	markdown: { remarkPlugins: [remarkReadingTime] }
+})
